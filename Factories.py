1b65f33eeae9a0904f57cd0f7b070f836c3b154a
Jon Boydell <jonboydell@hotmail.com>
2020-12-17 14:16:10 +0000

    added tests for factories/tick()

--- Factories.py
+++ Factories.py
@@ -2,12 +2,14 @@ import Resources as r
 
 
 class SteelFactory:
+    production_tick_threshold = 5
+
     def __init__(self, name):
         self.name = name
         self.producer = True
         self.consumer = True
-        self.production_tick_counter = 5
-        self.output = {}
+        self.production_tick_counter = self.production_tick_threshold
+        self.output = {r.steel: 0}
         self.input = {}
         self.production_threshold = {
             r.coal: 1,
@@ -30,6 +32,12 @@ class SteelFactory:
         else:
             self.input[resource] = 1
 
+    def increment_output(self, resource):
+        if resource in self.output:
+            self.output[resource] = self.output[resource] + 1
+        else:
+            self.output[resource] = 1
+
     def meets_production_threshold(self):
         if r.coal in self.input and r.iron in self.input:
             return self.input[r.coal] >= self.production_threshold[r.coal] \
@@ -38,11 +46,14 @@ class SteelFactory:
             return False
 
     def tick(self):
-        if self.meets_production_threshold() or self.is_consuming:
-            self.is_consuming = True
+        if self.meets_production_threshold() and not self.is_consuming:
             self.decrement_input(r.coal)
             self.decrement_input(r.iron)
+            self.is_consuming = True
+
+        if self.is_consuming:
             self.production_tick_counter -= 1
             if self.production_tick_counter <= 0:
                 self.is_consuming = False
-                self.production_tick_counter = 5
+                self.production_tick_counter = self.production_tick_threshold
+                self.increment_output(r.steel)
--- test_Factories.py
+++ test_Factories.py
@@ -3,8 +3,49 @@ import Resources as r
 from unittest import TestCase
 
 
-class TestMines(TestCase):
-    def test_increment_output_on_tick(self):
-      sf = SteelFactory('Steel Factory')
-      sf.increment_input(r.coal)
-      sf.increment_input(r.iron)
+class TestFactories(TestCase):
+    def test_meets_production_threshold(self):
+        sf = SteelFactory('Steel Factory')
+
+        sf.increment_input(r.coal)
+        sf.increment_input(r.iron)
+        self.assertTrue(sf.meets_production_threshold())
+
+        sf.decrement_input(r.coal)
+        self.assertFalse(sf.meets_production_threshold())
+
+        sf.decrement_input(r.iron)
+        self.assertFalse(sf.meets_production_threshold())
+
+        sf.increment_input(r.iron)
+        self.assertFalse(sf.meets_production_threshold())
+
+        sf.increment_input(r.coal)
+        self.assertTrue(sf.meets_production_threshold())
+
+    def test_tick(self):
+        sf = SteelFactory('Steel Factory')
+
+        sf.increment_input(r.coal)
+        sf.increment_input(r.iron)
+        self.assertTrue(sf.meets_production_threshold())
+
+        sf.tick()
+        self.assertTrue(sf.is_consuming)
+        self.assertTrue(0 == sf.input[r.coal] and 0 == sf.input[r.iron] and 0 == sf.output[r.steel])
+
+        sf.tick()
+        self.assertTrue(sf.is_consuming)
+        self.assertTrue(0 == sf.input[r.coal] and 0 == sf.input[r.iron] and 0 == sf.output[r.steel])
+
+        sf.tick()
+        self.assertTrue(sf.is_consuming)
+        self.assertTrue(0 == sf.input[r.coal] and 0 == sf.input[r.iron] and 0 == sf.output[r.steel])
+
+        sf.tick()
+        self.assertTrue(sf.is_consuming)
+        self.assertTrue(0 == sf.input[r.coal] and 0 == sf.input[r.iron] and 0 == sf.output[r.steel])
+
+        sf.tick()
+        self.assertFalse(sf.is_consuming)
+        self.assertTrue(0 == sf.input[r.coal] and 0 == sf.input[r.iron] and 1 == sf.output[r.steel])
